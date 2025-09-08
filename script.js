10ecc46c80771f1aa52442fd4f21dac3129cac38
TheRookiexe <mishrxa36@gmail.com>
2025-09-08 19:58:42 +0530

    Functions created for operations.

---
+++ script.js
@@ -0,0 +1,21 @@
+//==============functions======================//
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+let var1=0;
+let var2=0;
+let operator;
+
