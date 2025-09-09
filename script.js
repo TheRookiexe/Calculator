c690ed359b7cc1cb02a4360f46d410241b408894
TheRookiexe <mishrxa36@gmail.com>
2025-09-09 14:50:36 +0530

    Variables Created for all the Buttons in js.

--- script.js
+++ script.js
@@ -65,3 +65,36 @@ function operation(var1, var2, operator){
             break;
     }
 }
+
+//==========================Digits==============================//
+const display = document.querySelector('.screen');
+const zero_btn = document.getElementById('zero');
+const one_btn = document.getElementById('one');
+const two_btn = document.getElementById('two');
+const three_btn = document.getElementById('three');
+const four_btn = document.getElementById('four');
+const five_btn = document.getElementById('five');
+const six_btn = document.getElementById('six');
+const seven_btn = document.getElementById('seven');
+const eight_btn = document.getElementById('eight');
+const nine_btn = document.getElementById('nine');
+
+const zero = parseInt(zero_btn.value);
+const one = parseInt(one_btn.value);
+const two = parseInt(two_btn.value);
+const three = parseInt(three_btn.value);
+const four = parseInt(four_btn.value);
+const five = parseInt(five_btn.value);
+const six = parseInt(six_btn.value);
+const seven = parseInt(seven_btn.value);
+const eight = parseInt(eight_btn.value);
+const nine = parseInt(nine_btn.value);
+
+//=========================operators=====================================//
+const plus_btn = document.getElementById('plus');
+const multi_btn = document.getElementById('mul');
+const subtract_btn = document.getElementById('sub');
+const divide_btn = document.getElementById('divide');
+const power_btn = document.getElementById('power');
+const mod_btn = document.getElementById('modulos');
+const fact_btn = document.getElementById('factor');
